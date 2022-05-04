6e9a586d779f13c7f939cc4772526b5e9ddd23fd
unknown <eimdadulhaque@gmail.com>
2022-05-04 15:10:19 +0600

    create simple rdlc template

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,32 +1,44 @@
-﻿using CV_Maker_with_RDLC_Report_.Net_Core_MVC.Models;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Reporting.NETCore;
+using System.Data;
 using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
 
 namespace CV_Maker_with_RDLC_Report_.Net_Core_MVC.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private IWebHostEnvironment _environment;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(IWebHostEnvironment environment)
         {
-            _logger = logger;
+            _environment = environment;
         }
 
-        public IActionResult Index()
+        public IActionResult Print()
         {
-            return View();
+            string imgParam = "";
+            string imgPath = Path.Combine(_environment.WebRootPath, "Deep Blue.jpg");
+            string rdlcPath = Path.Combine(_environment.WebRootPath, "Report", "Report1.rdlc");
+
+            using (var bit = new Bitmap(imgPath))
+            {
+                using(var ms = new MemoryStream())
+                {
+                    bit.Save(ms, ImageFormat.Bmp);
+                    imgParam = Convert.ToBase64String(ms.ToArray());
+                }
+            }
+
+            var dt = new DataTable();
+            var report = new LocalReport();
+            report.DataSources.Add(new ReportDataSource("DataSet",dt));
+            report.ReportPath = rdlcPath;
+            var param = new[] {new ReportParameter("image", imgParam) };
+            report.SetParameters(param);
+            return File(report.Render("PDF"),"application/pdf","Rport.pdf");
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
